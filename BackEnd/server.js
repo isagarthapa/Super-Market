4620638f4a43fb2c788cd451617303f04bc8cf7a
Sagar Thapa <isagarthapa@gmail.com>
2024-08-25 22:39:38 +0530

    Added Production Environment condition

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -10,11 +11,21 @@ connectDB(); // Connect to MongoDB
 const app = express();
 
 
-
-app.get('/', (req, res) => {
-    res.send('API is running....');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/products', productRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/FrontEnd/build')));
+
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'FrontEnd', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

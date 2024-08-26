6ec84b1ff4854e263a10ac6e6c2b8e5e57b34180
Sagar Thapa <isagarthapa@gmail.com>
2024-08-26 13:58:32 +0530

    Updated production enivronment static path code

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -8,6 +9,8 @@ const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
  
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 
@@ -17,10 +20,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productRoutes);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/FrontEnd/build')));
+    app.use(express.static(path.join(__dirname, '..', 'FrontEnd', 'dist')));
 
     app.get('*', (req, res) => 
-        res.sendFile(path.resolve(__dirname, 'FrontEnd', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '..', 'FrontEnd', 'dist', 'index.html'))
     );
 } else {
     app.get('/', (req, res) => {
